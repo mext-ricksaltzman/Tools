72b32711a0ae788fcff5334a4d80f3ac0f05b89b
mext-ricksaltzman <ricksaltzman@mext.one>
2023-06-09 12:58:12 -0600

    comments for tbd

--- build_libmemcached.sh
+++ build_libmemcached.sh
@@ -3,4 +3,6 @@ cd libmemcached-1.0.18
 ./configure --enable-memaslap # --prefix=/usr/local
 sed -i 's/opt_servers == false/opt_servers == NULL/g' clients/memflush.cc
 make && make dist
-#sudo make install
+#make rpm # fails
+#sudo apt-get install libmemcached # does not contain memaslap
+#sudo make install # adds memaslap
