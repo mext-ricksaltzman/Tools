47e39dadfd1c312c11d557f7ced2671344f7f4b9
mext-ricksaltzman <ricksaltzman@mext.one>
2023-06-09 09:19:32 -0600

    build machine

---
+++ build_libmemcached.sh
@@ -0,0 +1,6 @@
+export LDFLAGS="-L/lib64 -lpthread"; 
+cd libmemcached-1.0.18
+./configure --enable-memaslap # --prefix=/usr/local
+sed -i 's/opt_servers == false/opt_servers == NULL/g' clients/memflush.cc
+make && make test
+sudo make install
---
+++ fetch_libmemcached.sh
@@ -0,0 +1,2 @@
+wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
+tar -zxf libmemcached-1.0.18.tar.gz
