dd67fd462fe7b8a604b815e65eced8fae61f3126
mext-ricksaltzman <ricksaltzman@mext.one>
2023-06-09 16:50:40 -0600

    install rpmbuilder

--- apt_gets.sh
+++ apt_gets.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y ansible
-sudo apt-get install -y make g++ gcc
+sudo apt-get install -y rpm make g++ gcc
 sudo apt-get install -y libevent-dev
 #sudo apt-get install -y libmemcached-tools
