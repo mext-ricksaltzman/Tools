5fc460940b8932478661245b4ff05195d89a58a9
Rick Saltzman <134726386+mext-ricksaltzman@users.noreply.github.com>
2023-06-09 13:11:35 -0600

    Update build_libmemcached.sh

--- build_libmemcached.sh
+++ build_libmemcached.sh
@@ -4,5 +4,5 @@ cd libmemcached-1.0.18
 sed -i 's/opt_servers == false/opt_servers == NULL/g' clients/memflush.cc
 make && make dist
 #make rpm # fails
-#sudo apt-get install libmemcached # does not contain memaslap
+#sudo apt-get install libmemcached-tools # does not contain memaslap
 #sudo make install # adds memaslap
