f43a2a09bc4576f9217a01f2a2284ec1514011f8
Rick Saltzman <134726386+mext-ricksaltzman@users.noreply.github.com>
2023-06-09 13:12:15 -0600

    Update build_libmemcached.sh

--- build_libmemcached.sh
+++ build_libmemcached.sh
@@ -1,4 +1,4 @@
-export LDFLAGS="-L/lib64 -lpthread"; 
+export LDFLAGS="-L/lib64 -lpthread"
 cd libmemcached-1.0.18
 ./configure --enable-memaslap # --prefix=/usr/local
 sed -i 's/opt_servers == false/opt_servers == NULL/g' clients/memflush.cc
