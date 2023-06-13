12cc51bfa33198c1ed72c6a7b90f1321c745f7a3
mext-ricksaltzman <ricksaltzman@mext.one>
2023-06-13 07:38:36 -0600

    add mysql-server

--- apt_gets.sh
+++ apt_gets.sh
@@ -3,3 +3,4 @@ sudo apt-get install -y libevent-dev
 sudo apt-get install -y memcached libmemcached-tools
 sudo apt-get install -y rpm make g++ gcc
 sudo apt-get install -y libsasl2-2 libsasl2-modules
+sudo apt-get install -y mysql-server
