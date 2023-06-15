6a7e5625511975c603684edd909a2607a3cfb689
mext-ricksaltzman <ricksaltzman@mext.one>
2023-06-15 14:21:33 -0600

    executable from build dir only

--- build_memaslap.sh
+++ build_memaslap.sh
@@ -1,28 +1,16 @@
 # libmemcached tests require memcached
-sudo systemctl restart memcached
+# sudo systemctl restart memcached
 # https://medium.com/swlh/the-complete-guide-to-benchmark-the-performance-of-memcached-on-ubuntu-16-04-71edeaf6e740
 # as per build instructions above 
 export LDFLAGS="-L/lib64 -lpthread"
-# set up for package
-mkdir -p ${HOME}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 # fetched previously
 cd libmemcached-1.0.18
-# from this repo add makefile and spec for memaslap
-cp ../memaslap.* .
 # as per build instructions
 ./configure --enable-memaslap
 # fix up to ensure compile succeeds
-sed -i 's/opt_servers == false/opt_servers == NULL/g' clients/memflush.cc
-# build rpm
-make -f memaslap.makefile >> make.log
-
-# test
-# make check >> make.log
-# install
-# sudo make install >> make.log
-# remove dependencies on existing elements in /usr/bin
-# sed -i 's/BuildRequires/\#BuildRequires/g' support/libmemcached.spec
-# do not make debug rpm, unresolved issues
-# sed -i 's/libmemcached-debuginfo/libmemcached-devel/' Makefile
-#package
-#make rpm  >> make.log
+# sed -i 's/opt_servers == false/opt_servers == NULL/g' clients/memflush.cc
+# build memaslap
+make clients/memaslap
+sudo ln -fs `pwd`/clients/memaslap /usr/bin
+# set up for package
+# mkdir -p ${HOME}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
