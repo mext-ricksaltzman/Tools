63c753947df6c1c82cc478a1bf9ea972920a9dd3
mext-ricksaltzman <ricksaltzman@mext.one>
2023-06-15 21:16:44 -0600

    build.sh and makefile for memaslap

--- build_memaslap.sh
+++ build_memaslap.sh
@@ -3,14 +3,5 @@
 # https://medium.com/swlh/the-complete-guide-to-benchmark-the-performance-of-memcached-on-ubuntu-16-04-71edeaf6e740
 # as per build instructions above 
 export LDFLAGS="-L/lib64 -lpthread"
-# fetched previously
-cd libmemcached-1.0.18
-# as per build instructions
-./configure --enable-memaslap
-# fix up to ensure compile succeeds
-# sed -i 's/opt_servers == false/opt_servers == NULL/g' clients/memflush.cc
-# build memaslap
-make clients/memaslap
-sudo ln -fs `pwd`/clients/memaslap /usr/bin
-# set up for package
+make -f makefile.memaslap
 # mkdir -p ${HOME}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
--- fetch_libmemcached.sh
+++
@@ -1,3 +0,0 @@
-# version 1.0.18
-wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
-tar -zxf libmemcached-1.0.18.tar.gz
