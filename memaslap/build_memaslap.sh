e7965a568708c10be682f01cc0bb0c7656e7a3bb
mext-ricksaltzman <ricksaltzman@mext.one>
2023-06-17 10:05:43 -0600

    switch to Makefile

--- memaslap/build_memaslap.sh
+++ memaslap/build_memaslap.sh
@@ -2,6 +2,8 @@
 # sudo systemctl restart memcached
 # https://medium.com/swlh/the-complete-guide-to-benchmark-the-performance-of-memcached-on-ubuntu-16-04-71edeaf6e740
 # as per build instructions above 
-export LDFLAGS="-L/lib64 -lpthread"
-make -f makefile.memaslap
-# mkdir -p ${HOME}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+make
+#mkdir -p ${HOME}/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+#cp *spec ${HOME}/rpmbuild/SPECS
+#cp *gz ${HOME}/rpmbuild/SOURCES
+#rpmbuild -ba ${HOME}/rpmbuild/SPECS/memaslap.spec
